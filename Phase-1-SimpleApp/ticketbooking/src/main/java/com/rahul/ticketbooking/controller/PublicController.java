ddf4bbb909c572d2519eae0f3b95bcd640f3d86b
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-25 20:50:15 +0530

    Simple controller

--- Phase-1-SimpleApp/ticketbooking/src/main/java/com/rahul/ticketbooking/controller/PublicController.java
+++ Phase-1-SimpleApp/ticketbooking/src/main/java/com/rahul/ticketbooking/controller/PublicController.java
@@ -1,4 +1,17 @@
 package com.rahul.ticketbooking.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
 public class PublicController {
+
+    @GetMapping
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.status(HttpStatus.OK).body("Hello, How are you?");
+    }
 }
