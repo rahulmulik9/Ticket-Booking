d144d0bda1ee849a2c6854c72545fb46d1b6a63e
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-26 18:50:10 +0530

    Updated code

--- Phase-1-SimpleApp/ticketbooking/src/main/java/com/rahul/ticketbooking/entity/Movie.java
+++ Phase-1-SimpleApp/ticketbooking/src/main/java/com/rahul/ticketbooking/entity/Movie.java
@@ -1,5 +1,6 @@
 package com.rahul.ticketbooking.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +26,17 @@ public class Movie {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    //Lazy means the data was never loaded from the DB in the first place — it's fetched only when something asks for it.
+    //When Jackson calls getShows() during serialization, JPA tries to go fetch it right then —
+    //but the DB session is already closed by that point, so the fetch itself fails. That's the actual exception.
+    //@JsonIgnore just tells Jackson: "Don't call getShows() at all when converting this to JSON. Skip it.
+
     @OneToMany(mappedBy = "movie", fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Show> shows = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
